325baee0d3dcd763711a39dd4724a3876d28f931
Lutz Rossa <34568681+lrossa@users.noreply.github.com>
2019-10-21 06:29:39 +0200

    moved SHALL libraries to: https://github.com/SampleEnvironment/SHALL
    Signed-off-by: Lutz Rossa <34568681+lrossa@users.noreply.github.com>

--- SHALL/SHALL-ClientDummy/main.h
+++
@@ -1,8 +0,0 @@
-/*
-SPDX-License-Identifier: LGPL-3.0-or-later
-Copyright (c) 2017-2019 Helmholtz-Zentrum Berlin fuer Materialien und Energie GmbH <https://www.helmholtz-berlin.de>
-*/
-#include <QApplication>
-#include <stdio.h>
-#include "exports.h"
-
